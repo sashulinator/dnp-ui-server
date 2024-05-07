db33fa1360c8550267952a26a6278c39e242cdf2
Savchenko Alexander <asavchenko@datatech.ru>
2024-05-07 10:14:18 +0400

    add translation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TranslationsModule } from './translations/translations.module';
 
 @Module({
-  imports: [],
+  imports: [TranslationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
