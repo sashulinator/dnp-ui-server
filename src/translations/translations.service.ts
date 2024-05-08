fe02a8d87cb0eb8083296f0364fbbea79b9cc21c
Savchenko Alexander <asavchenko@datatech.ru>
2024-05-08 11:47:22 +0400

    refactoring translation.service

--- src/translations/translations.service.ts
+++ src/translations/translations.service.ts
@@ -1,65 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma, Translation } from '@prisma/client';
-import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common'
+import { Prisma, Translation } from '@prisma/client'
+import { PrismaService } from '../prisma.service'
 
-const TAKE = 100;
+const TAKE = 100
 
 @Injectable()
 export class TranslationsService {
   constructor(private prisma: PrismaService) {}
 
   /**
+   * GET
+   */
+
+  async getFirst(translationWhereUniqInput: Prisma.TranslationWhereUniqueInput): Promise<Translation> {
+    return this.prisma.translation.findFirstOrThrow({
+      where: translationWhereUniqInput,
+    })
+  }
+
+  async getUniq(translationWhereUniqInput: Prisma.TranslationWhereUniqueInput): Promise<Translation> {
+    return this.prisma.translation.findUniqueOrThrow({
+      where: translationWhereUniqInput,
+    })
+  }
+
+  /**
    * FIND
    */
 
-  findFirst(
-    translationWhereInput: Prisma.TranslationWhereInput,
-  ): Promise<Translation | null> {
+  async findFirst(translationWhereInput: Prisma.TranslationWhereInput): Promise<Translation | null> {
     return this.prisma.translation.findFirst({
       where: translationWhereInput,
-    });
+    })
   }
 
-  findUnique(
-    translationWhereUniqInput: Prisma.TranslationWhereUniqueInput,
-  ): Promise<Translation | null> {
+  async findUnique(translationWhereUniqInput: Prisma.TranslationWhereUniqueInput): Promise<Translation | null> {
     return this.prisma.translation.findUnique({
       where: translationWhereUniqInput,
-    });
+    })
   }
 
-  getFirst(
-    translationWhereUniqInput: Prisma.TranslationWhereUniqueInput,
-  ): Promise<Translation> {
-    return this.prisma.translation.findFirstOrThrow({
-      where: translationWhereUniqInput,
-    });
-  }
-
-  getUniq(
-    translationWhereUniqInput: Prisma.TranslationWhereUniqueInput,
-  ): Promise<Translation> {
-    return this.prisma.translation.findUniqueOrThrow({
-      where: translationWhereUniqInput,
-    });
-  }
-
-  create(data: Prisma.TranslationCreateInput) {
-    return this.prisma.translation.create({
-      data,
-    });
-  }
-
-  findAll(
+  async findAll(
     params: {
-      skip?: number;
-      take?: number;
-      cursor?: Prisma.TranslationWhereUniqueInput;
-      where?: Prisma.TranslationWhereInput;
-      orderBy?: Prisma.TranslationOrderByWithRelationInput;
-    } = {},
+      skip?: number
+      take?: number
+      cursor?: Prisma.TranslationWhereUniqueInput
+      where?: Prisma.TranslationWhereInput
+      orderBy?: Prisma.TranslationOrderByWithRelationInput
+    } = {}
   ): Promise<[Translation[], number]> {
-    const { skip, take = TAKE, cursor, where, orderBy } = params;
+    const { skip, take = TAKE, cursor, where, orderBy } = params
 
     const args = {
       skip,
@@ -67,27 +57,27 @@ export class TranslationsService {
       cursor,
       where,
       orderBy,
-    };
+    }
 
-    return this.prisma.$transaction([
-      this.prisma.translation.findMany(args),
-      this.prisma.translation.count(args),
-    ]);
+    return this.prisma.$transaction([this.prisma.translation.findMany(args), this.prisma.translation.count(args)])
   }
 
-  update(
-    where: Prisma.TranslationWhereUniqueInput,
-    data: Prisma.TranslationUpdateInput,
-  ): Promise<Translation> {
+  async create(data: Prisma.TranslationCreateInput) {
+    return this.prisma.translation.create({
+      data,
+    })
+  }
+
+  async update(where: Prisma.TranslationWhereUniqueInput, data: Prisma.TranslationUpdateInput): Promise<Translation> {
     return this.prisma.translation.update({
       data,
       where,
-    });
+    })
   }
 
-  remove(where: Prisma.TranslationWhereUniqueInput): Promise<Translation> {
+  async remove(where: Prisma.TranslationWhereUniqueInput): Promise<Translation> {
     return this.prisma.translation.delete({
       where,
-    });
+    })
   }
 }
