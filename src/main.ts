a1fffceb0579a608a7ad1b04f44d71ee8b7b0a17
Savchenko Alexander <asavchenko@datatech.ru>
2024-06-01 20:36:30 +0400

    chore(env): добавить PORT

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(port)
+  console.log(`Started on http://localhost:${port}`)
 }
 bootstrap()
