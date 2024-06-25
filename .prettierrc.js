d2af38a998faeb9f44db12b012792d4993520cc6
Alexander Savchenko <asavchenko@datatech.ru>
2024-06-25 14:09:02 +0400

    chore(package.json): добавить eslint-prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
   semi: false,
   trailingComma: 'es5',
   singleQuote: true,
@@ -7,8 +6,4 @@ module.exports = {
   printWidth: 120,
   tabWidth: 2,
   endOfLine: 'auto',
-  importOrder: ['reset.css$', 'css$', '^@', '^[a-z]', '^~', '^.'],
-  importOrderSeparation: true,
-  importOrderParserPlugins: ["typescript", "decorators-legacy"],
-  importOrderSortSpecifiers: true,
 }
