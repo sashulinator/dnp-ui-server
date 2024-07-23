4df406f0404577b9fe3896e87b51205aa2ce4062
Savchenko Alexander <asavchenko@datatech.ru>
2024-07-23 19:18:32 +0700

    fix(NormalizationConfig): полностью DGR-150

--- normalization-config.ts
+++ normalization-config.ts
@@ -57,11 +57,11 @@ export const normalizationConfigSchema = v.object({
   id: v.pipe(v.string(), v.nonEmpty()),
   v: v.pipe(v.number(), v.notValue(0)),
   name: v.pipe(v.string(), v.nonEmpty()),
+  current: v.boolean(),
   createdBy: v.pipe(v.string(), v.nonEmpty()),
   updatedBy: v.pipe(v.string(), v.nonEmpty()),
   createdAt: v.pipe(v.string(), v.nonEmpty()),
   updatedAt: v.pipe(v.string(), v.nonEmpty()),
-  sourceConfigKeyName: v.pipe(v.string(), v.nonEmpty()),
   data: v.object({
     executables: v.array(executableSchema),
     sdk: sdkSchema,
@@ -97,7 +97,6 @@ export const updateNormalizationConfigSchema = v.omit(normalizationConfigSchema,
   'updatedAt',
   'createdBy',
   'updatedBy',
-  'id',
   'v',
 ])
 
