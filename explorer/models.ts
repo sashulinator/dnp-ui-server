402989199c0cbbcc73553369de45eee22442e220
Savchenko Alexander <asavchenko@datatech.ru>
2024-07-28 16:20:30 +0700

    feat(explorer-Viewer): создать компонент DGR-164

--- explorer/models.ts
+++ explorer/models.ts
@@ -6,18 +6,45 @@ import * as v from 'valibot'
 
 export const explorerSchema = v.object({
   name: v.pipe(v.string(), v.nonEmpty()),
-  type: v.union([v.literal('jdbc'), v.literal('s3'), v.literal('table')]),
-  items: v.union([v.array(v.lazy(() => jdbcItemSchema))]),
+  type: v.lazy(() => typeSchema),
+  items: v.array(v.lazy(() => itemSchema)),
 })
 
 export type Explorer = v.InferOutput<typeof explorerSchema>
+
+/**
+ * Path
+ */
+
+export const pathSchema = v.object({
+  name: v.pipe(v.string(), v.nonEmpty()),
+  type: v.lazy(() => typeSchema),
+})
+
+export type Path = v.InferOutput<typeof pathSchema>
+
+/**
+ * Item
+ */
+
+export const itemSchema = v.union([v.lazy(() => jdbcItemSchema)])
+
+export type Item = v.InferOutput<typeof itemSchema>
+
+/**
+ * type
+ */
+
+export const typeSchema = v.union([v.literal('jdbc'), v.literal('s3'), v.literal('table'), v.literal('record')])
+
+export type Type = v.InferOutput<typeof typeSchema>
 
 /**
  * JdbcItem
  */
 
 export const jdbcItemSchema = v.object({
-  type: v.union([v.literal('table'), v.literal('record')]),
+  type: typeSchema,
   name: v.pipe(v.string(), v.nonEmpty()),
   data: v.union([v.object({}), v.never()]),
 })
