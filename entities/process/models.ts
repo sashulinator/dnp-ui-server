3d17f97d0b2221055c18d4d94190494d0342d8f4
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 17:06:38 +0700

    chore(Process): добавить связи DGR-178

--- entities/process/models.ts
+++ entities/process/models.ts
@@ -1,22 +1,42 @@
 import * as v from 'valibot'
+import { normalizationConfigSchema } from '../normalization-config'
 
 /**
- * Process
+ * BaseProcess
  */
 
-export const processSchema = v.object({
+export const baseProcessSchema = v.object({
   id: v.pipe(v.string(), v.nonEmpty()),
   createdBy: v.pipe(v.string(), v.nonEmpty()),
   createdAt: v.pipe(v.string(), v.nonEmpty()),
   normalizationConfigId: v.pipe(v.string(), v.nonEmpty()),
 })
 
+export type BaseProcess = v.InferOutput<typeof baseProcessSchema>
+
+/**
+ * Relations
+ */
+
+export const processRelationsSchema = v.object({
+  normalizationConfig: normalizationConfigSchema,
+})
+
+export type ProcessRelations = v.InferOutput<typeof processRelationsSchema>
+
+/**
+ * Process
+ */
+
+export const processSchema = v.intersect([baseProcessSchema, processRelationsSchema])
+
 export type Process = v.InferOutput<typeof processSchema>
 
 /**
  * CreateProcess
  */
-export const createProcessSchema = v.omit(processSchema, ['id', 'createdAt', 'createdBy'])
+
+export const createProcessSchema = v.omit(baseProcessSchema, ['id', 'createdAt', 'createdBy'])
 
 export type CreateProcess = v.InferOutput<typeof createProcessSchema>
 
@@ -24,6 +44,6 @@ export type CreateProcess = v.InferOutput<typeof createProcessSchema>
  * UpdateProcess
  */
 
-export const updateProcessSchema = v.omit(processSchema, ['createdAt', 'createdBy'])
+export const updateProcessSchema = v.omit(baseProcessSchema, ['createdAt', 'createdBy'])
 
 export type UpdateProcess = v.InferOutput<typeof updateProcessSchema>
