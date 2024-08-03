bf7fe6b5c3641a1cba1046e658aef9b59b776a1f
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 17:19:57 +0700

    chore(lib/prisma/types/dto-sync): добавить NormalizationConfig DGR-178

---
+++ src/lib/prisma/types/_dto-sync/normalization-config.ts
@@ -0,0 +1,7 @@
+//            ↓
+import { type NormalizationConfig as PrismaType } from '@prisma/client'
+//            ↓
+import { type BaseNormalizationConfig as DtoType } from '~/entities/normalization-configs/dto'
+import { type DateToString, check } from './_private'
+
+check<DtoType, PrismaType>({} as DtoType, {} as DateToString<PrismaType>)
