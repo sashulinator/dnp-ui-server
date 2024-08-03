2efd7e2e62dbdd5ec6f047d53ff22a02df81693f
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 16:53:00 +0700

    chore(lib/prisma/types/dto-sync): добавить TargetTable DGR-178

---
+++ src/lib/prisma/types/_dto-sync/target-table.ts
@@ -0,0 +1,7 @@
+//            ↓
+import { type TargetTable as PrismaType } from '@prisma/client'
+//            ↓
+import { type BaseTargetTable as DtoType } from '~/entities/target-table/dto'
+import { type DateToString, check } from './_private'
+
+check<DtoType, PrismaType>({} as DtoType, {} as DateToString<PrismaType>)
