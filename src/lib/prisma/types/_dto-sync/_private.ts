b74b143a7959ca7d9658e6be96a35badfeb40ff0
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 16:49:01 +0700

    chore(lib/prisma/types): добавить тип check DGR-178

---
+++ src/lib/prisma/types/_dto-sync/_private.ts
@@ -0,0 +1,16 @@
+import { type JsonValue } from '@prisma/client/runtime/library'
+
+export function check<Type extends Record<string, unknown>, PrismaType extends Record<string, unknown>>(
+  prismatype: DateToString<PrismaType>,
+  type: ObjectToJsonValue<Type>
+) {
+  return { type, ptype: prismatype }
+}
+
+export type DateToString<T> = {
+  [K in keyof T]: T[K] extends Date ? string : T[K]
+}
+
+export type ObjectToJsonValue<T> = {
+  [K in keyof T]: T[K] extends Record<string, unknown> ? JsonValue : T[K]
+}
