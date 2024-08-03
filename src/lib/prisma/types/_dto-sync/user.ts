5c3ac09fcd1231af369ff74b5d104bacc56973b1
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 17:12:21 +0700

    chore(lib/prisma/types/dto-sync): добавить User DGR-178

---
+++ src/lib/prisma/types/_dto-sync/user.ts
@@ -0,0 +1,7 @@
+//            ↓
+import { type User as PrismaType } from '@prisma/client'
+//            ↓
+import { type BaseUser as DtoType } from '~/entities/user/dto'
+import { type DateToString, check } from './_private'
+
+check<DtoType, PrismaType>({} as DtoType, {} as DateToString<PrismaType>)
