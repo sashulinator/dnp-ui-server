f5d78ced56bb2415cde7a93d5a7bc3ba1dd67418
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-03 16:52:37 +0700

    chore(lib/prisma/types/dto-sync): добавить StoreConfig DGR-178

---
+++ src/lib/prisma/types/_dto-sync/store-config.ts
@@ -0,0 +1,7 @@
+//            ↓
+import { type StoreConfig as PrismaType } from '@prisma/client'
+//            ↓
+import { type BaseStoreConfig as DtoType } from '~/entities/store-configs/dto'
+import { type DateToString, check } from './_private'
+
+check<DtoType, PrismaType>({} as DtoType, {} as DateToString<PrismaType>)
