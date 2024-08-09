ef922effb2d224947df992b4d875c31b6b9094dd
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-09 18:22:47 +0700

    refactor(OperationalTable): explorer service DGR-197

--- entities/explorer/models.ts
+++ entities/explorer/models.ts
@@ -5,12 +5,33 @@ import * as v from 'valibot'
  */
 
 export const explorerSchema = v.object({
+  paths: v.array(v.lazy(() => pathSchema)),
   name: v.pipe(v.string(), v.nonEmpty()),
   type: v.lazy(() => typeSchema),
   items: v.array(v.lazy(() => itemSchema)),
 })
 
 export type Explorer = v.InferOutput<typeof explorerSchema>
+
+/**
+ * Item
+ */
+
+export const itemSchema = v.object({
+  type: v.lazy(() => typeSchema),
+  name: v.pipe(v.string(), v.nonEmpty()),
+  data: v.union([v.object({}), v.never()]),
+})
+
+export type Item = v.InferOutput<typeof itemSchema>
+
+/**
+ * type
+ */
+
+export const typeSchema = v.union([v.literal('jdbc'), v.literal('s3'), v.literal('table'), v.literal('record')])
+
+export type Type = v.InferOutput<typeof typeSchema>
 
 /**
  * Path
@@ -24,29 +45,14 @@ export const pathSchema = v.object({
 export type Path = v.InferOutput<typeof pathSchema>
 
 /**
- * Item
+ * StoreConfig
  */
 
-export const itemSchema = v.union([v.lazy(() => jdbcItemSchema)])
-
-export type Item = v.InferOutput<typeof itemSchema>
-
-/**
- * type
- */
-
-export const typeSchema = v.union([v.literal('jdbc'), v.literal('s3'), v.literal('table'), v.literal('record')])
-
-export type Type = v.InferOutput<typeof typeSchema>
-
-/**
- * JdbcItem
- */
-
-export const jdbcItemSchema = v.object({
-  type: typeSchema,
-  name: v.pipe(v.string(), v.nonEmpty()),
-  data: v.union([v.object({}), v.never()]),
+export const storeConfigSchema = v.object({
+  host: v.pipe(v.string(), v.nonEmpty()),
+  port: v.pipe(v.string(), v.nonEmpty()),
+  username: v.pipe(v.string(), v.nonEmpty()),
+  password: v.pipe(v.string(), v.nonEmpty()),
 })
 
-export type JdbcItem = v.InferOutput<typeof jdbcItemSchema>
+export type StoreConfig = v.InferOutput<typeof storeConfigSchema>
