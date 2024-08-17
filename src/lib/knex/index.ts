172a210c5919d98e2d34bdde42405daaca1d8249
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-17 16:11:02 +0700

    feat(lib/knex): создать PostgresHelper DGR-0

---
+++ src/lib/knex/index.ts
@@ -0,0 +1,2 @@
+export { PostgresHelper } from './postgres-helper'
+export { TableHelper } from './table-helper'
---
+++ src/lib/knex/postgres-helper.ts
@@ -0,0 +1,67 @@
+import knex, { type Knex } from 'knex'
+
+interface ConnectionConfig {
+  host: string
+  port: string
+  username: string
+  password: string
+}
+
+export class PostgresHelper {
+  knex: Knex<unknown, unknown[]>
+
+  constructor(
+    protected connectionConfig: ConnectionConfig,
+    protected database: string
+  ) {
+    this.knex = knex({
+      client: 'postgres',
+      connection: {
+        user: connectionConfig.username,
+        password: connectionConfig.password,
+        port: parseInt(connectionConfig.port),
+        host: connectionConfig.host,
+        database: database,
+      },
+    })
+  }
+
+  async getPrimaryKey(tableName: string) {
+    const ret = await this.knex.raw<{ rows: { attname: string }[] }>(`
+        SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type
+        FROM   pg_index i
+        JOIN   pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
+        WHERE  i.indrelid = '"${tableName}"'::regclass
+        AND    i.indisprimary;
+    `)
+
+    return ret.rows[0].attname
+  }
+
+  async findManyTables(params: {
+    limit?: number
+    offset?: number
+  }): Promise<{ tablename: string; schemaname: string }[]> {
+    type Item = { tablename: string; schemaname: string }
+    const queryBuilder = this.knex<Item[], Item[]>('pg_tables')
+    const ret = await queryBuilder.where('schemaname', '=', 'public').limit(params.limit).offset(params.offset)
+    return ret
+  }
+
+  async countTables(): Promise<number> {
+    type Item = { tablename: string; schemaname: string }
+    const queryBuilder = this.knex<Item[], Item[]>('pg_tables')
+    const ret = await queryBuilder.where('schemaname', '=', 'public').count('*')
+    return parseInt((ret as unknown as { count: string }[])[0].count)
+  }
+
+  async findManyAndCountTables(params: {
+    limit?: number
+    offset?: number
+  }): Promise<[{ tablename: string; schemaname: string }[], number]> {
+    const findManyPromise = this.findManyTables(params)
+    const countPromise = this.countTables()
+
+    return Promise.all([findManyPromise, countPromise])
+  }
+}
