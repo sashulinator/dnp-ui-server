fd8369fb6aaf716198021a669ca6f8673318c741
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-17 19:20:52 +0700

    feat(OperationalTable): создание промежуточных таблиц в бд DGR-207

--- src/entities/operational-table/service.ts
+++ src/entities/operational-table/service.ts
@@ -4,6 +4,9 @@ import PrismaService from '../../shared/prisma/service'
 import ExplorerService, { type ExploreParams as ExplorerExploreParams } from '../explorer/service'
 import { CrudService } from '~/shared/crud-service'
 import type { StoreConfig } from '../store-configs/dto'
+import { TableHelper } from '~/lib/knex'
+import { type CreateTableSchemaItem } from '~/lib/knex/table-helper'
+import { createFromStoreConfig } from '~/lib/knex'
 
 export type OperationalTable = PrismaOperationalTable
 export type CreateOperationalTable = Prisma.OperationalTableUncheckedCreateInput
@@ -53,7 +56,7 @@ export default class OperationalTableService extends CrudService<
 
   async explore(params: ExploreParams) {
     const operationlTable = await this.getUnique({ where: { kn: params.kn } })
-    const storeConfig = (await this.getStoreConfig()) as unknown as StoreConfig
+    const storeConfig = await this.getStoreConfig()
 
     const exploreParams: Required<ExplorerExploreParams> = {
       take: params.take || 100,
@@ -71,11 +74,46 @@ export default class OperationalTableService extends CrudService<
     return this.explorerService.expore(exploreParams)
   }
 
-  async getStoreConfig() {
+  /**
+   * В системе для промежуточной таблицы должна быть создана конфигурация хранилица,
+   * в которой указаны параметры подключения к базе данных
+   * @returns {Promise<StoreConfig>}
+   */
+  async getStoreConfig(): Promise<StoreConfig> {
     const storeConfig = await this.prisma.storeConfig.findUnique({ where: { kn: 'operational-tables' } })
 
     if (!storeConfig) throw new HttpException('Create StoreConfig with kn="operational-tables"', HttpStatus.NOT_FOUND)
 
-    return storeConfig
+    return storeConfig as unknown as StoreConfig
+  }
+
+  async create(params: {
+    data: CreateOperationalTable
+    select?: Select
+    include?: Include
+  }): Promise<OperationalTable> {
+    const storeConfig = await this.getStoreConfig()
+    const tableSchema = params.data.tableSchema as { name: string }[]
+
+    const newSchema: CreateTableSchemaItem[] = tableSchema.map((item) => ({
+      type: 'string' as const,
+      params: [item.name],
+    }))
+
+    const knex = createFromStoreConfig(storeConfig.data, storeConfig.data.database)
+
+    return this.prisma.$transaction(async (prismaTrx) => {
+      return knex.transaction(async (knexTrx) => {
+        const tableHelper = new TableHelper(knexTrx, params.data.tableName)
+
+        await tableHelper.createTable([
+          { type: 'increments', params: ['id', { primaryKey: true }] },
+          { type: 'string', params: ['_status'], defaultTo: '0' },
+          ...newSchema,
+        ])
+
+        return prismaTrx.operationalTable.create(this._prepareSelectIncludeParams(params))
+      })
+    })
   }
 }
