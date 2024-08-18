73339047f746d0437cc86b1cfcdedc3fa578fe81
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-18 15:51:36 +0700

    feat(OperationalTable): типизировать tableSchema DGR-200

--- entities/operational-table/models.ts
+++ entities/operational-table/models.ts
@@ -12,7 +12,7 @@ export const baseOperationalTableSchema = v.object({
   name: v.string(),
   nav: v.boolean(),
   tableName: v.string(),
-  tableSchema: v.array(v.looseObject({})),
+  tableSchema: v.lazy(() => tableSchemaSchema),
   // meta
   ...crudableSchema.entries,
 })
@@ -53,3 +53,33 @@ export type CreateOperationalTable = v.InferOutput<typeof createOperationalTable
 export const updateOperationalTableSchema = v.omit(baseOperationalTableSchema, getObjectKeys(crudableSchema.entries))
 
 export type UpdateOperationalTable = v.InferOutput<typeof updateOperationalTableSchema>
+
+/**
+ * TableSchema
+ */
+
+export const tableSchemaSchema = v.object({
+  defaultView: v.union([v.literal('tree'), v.literal('table')]),
+  items: v.array(v.lazy(() => tableSchemaItemSchema)),
+})
+
+export type TableSchema = v.InferOutput<typeof tableSchemaSchema>
+
+/**
+ * TableSchemaItem
+ */
+
+export const tableSchemaItemSchema = v.object({
+  id: v.string(),
+  name: v.string(),
+  key: v.string(),
+  type: v.string(),
+  relation: v.optional(
+    v.object({
+      key: v.string(),
+      table: v.string(),
+    })
+  ),
+})
+
+export type TableSchemaItem = v.InferOutput<typeof tableSchemaItemSchema>
