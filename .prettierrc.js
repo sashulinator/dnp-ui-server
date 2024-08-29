c71bdce78646275604377ba76e53af41b523de47
Savchenko Alexander <asavchenko@datatech.ru>
2024-08-29 11:48:36 +0700

    chore(prettier): использовать @trivago/prettier-plugin-sort-imports DGR-0

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,13 @@
 module.exports = {
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   semi: false,
-  trailingComma: 'es5',
+  trailingComma: 'all',
   singleQuote: true,
   jsxSingleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   endOfLine: 'auto',
+  importOrder: ['reset.css$', 'css$', '^@', '^[a-z]', '^~', '^.'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 }
