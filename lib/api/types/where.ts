73efa9fea24ee9737c4aadc6ca46e8a173bd34eb
Savchenko Alexander <asavchenko@datatech.ru>
2024-09-04 12:42:51 +0700

    feat(common/api/where): дописать DGR-208

--- lib/api/types/where.ts
+++ lib/api/types/where.ts
@@ -5,36 +5,68 @@ export type Where<T extends Record<string, unknown> = Record<string, unknown>> =
 } & ReplaceValueByFilter<T>
 
 export type ReplaceValueByFilter<T> = {
-  [K in keyof T]?: T[K] extends string ? StringFilter : T[K] extends number ? IntFilter : unknown
+  [K in keyof T]?: T[K] extends string
+    ? StringFilter
+    : T[K] extends number
+      ? IntFilter
+      : T[K] extends boolean
+        ? BooleanFilter
+        : unknown
 }
 
-export type StringFilter = {
-  equals?: string
-  // in?: string[]
-  // notIn?: string[]
-  lt?: string
-  lte?: string
-  gt?: string
-  gte?: string
+export type StringFilter = ComparisonFilter<string> &
+  InFilter<string> &
+  MatchFilter &
+  QueryModeFilter & {
+    not?: (ComparisonFilter<string> & InFilter<string> & MatchFilter & QueryModeFilter) | string
+  }
+
+export type IntFilter = ComparisonFilter<number> &
+  InFilter<number> & {
+    not?: ComparisonFilter<number> & InFilter<number>
+  }
+
+export type BooleanFilter = {
+  equals?: boolean
+  not?: boolean
+}
+
+export interface QueryModeFilter {
+  mode?: QueryMode
+}
+
+export interface ComparisonFilter<T extends string | number> {
+  equals?: T
+  lt?: T
+  lte?: T
+  gt?: T
+  gte?: T
+}
+
+export interface InFilter<T extends string | number> {
+  in?: T[]
+  notIn?: T[]
+}
+
+export interface MatchFilter {
   contains?: string
   startsWith?: string
   endsWith?: string
-  not?: StringFilter | string
 }
 
-export type IntFilter = {
-  equals?: number
-  in?: number[]
-  notIn?: number[]
-  lt?: number
-  lte?: number
-  gt?: number
-  gte?: number
-  not?: IntFilter
+export const StringFilterPrimitive = {
+  equals: 'equals',
+  lt: 'lt',
+  lte: 'lt',
+  gt: 'gt',
+  gte: 'gte',
+  contains: 'contains',
+  startsWith: 'startsWith',
 }
 
-// На будущее
-// export type BoolFilter = {
-//   equals?: boolean
-//   not?: boolean
-// }
+export const QUERY_MODE = {
+  DEFAULT: 'default',
+  INSENSITIVE: 'insensitive',
+}
+
+export type QueryMode = (typeof QUERY_MODE)[keyof typeof QUERY_MODE]
