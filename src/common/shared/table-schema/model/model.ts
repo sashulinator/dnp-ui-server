e3a0177ffcedadb0e60e759dae1571b27835ba8a
Pavel Epifanov <PEpifanov@inno.tech>
2024-09-11 16:57:57 +0300

    refactor(table-schema): вынести интерфейс Schema в shared DGR-245

--- src/common/shared/table-schema/model/model.ts
+++ src/common/shared/table-schema/model/model.ts
@@ -1,14 +1,17 @@
 import * as v from 'valibot'
 
+/**
+ * Schema
+ */
+
 export const schemaModel = v.object({
-  // defaultView: v.union([v.literal('tree'), v.literal('table')]),
   items: v.array(v.lazy(() => schemaItemModel)),
 })
 
 export type Schema = v.InferOutput<typeof schemaModel>
 
 /**
- * TableModelItem
+ * SchemaItem
  */
 
 export const schemaItemModel = v.object({
@@ -21,7 +24,6 @@ export const schemaItemModel = v.object({
   type: v.string(),
   relation: v.optional(
     v.object({
-      type: v.string(),
       columnName: v.string(),
       kn: v.string(),
     }),
