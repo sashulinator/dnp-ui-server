2eafc955bff773b0e2e337bef8a6ce0e0eb67ea9
Oleg Poroshin <oaporoshin@vtb.ru>
2024-09-20 14:37:41 +0300

    feat(operational-table-import): обновление модели Process DGR-213

--- prisma/seed/processes.ts
+++ prisma/seed/processes.ts
@@ -1,12 +1,13 @@
-import { type Prisma, type PrismaClient } from '@prisma/client'
-import { normalizationConfig1 } from './normalization-configs'
 import { createId } from '@paralleldrive/cuid2'
+import { type Prisma, type PrismaClient } from '@prisma/client'
+
 import { users } from './users'
 
 const processes1: Prisma.ProcessUncheckedCreateInput = {
   id: 'tz4a98xxat96iws9zmbrgj3a',
   createdById: users[0].id,
-  normalizationConfigId: normalizationConfig1.id,
+  type: 'import',
+  data: '',
 }
 
 export async function seedProcesses(prisma: PrismaClient) {
