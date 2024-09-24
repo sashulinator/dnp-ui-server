d4a3d317dde7b37e2167b9643d741dd824741997
Pavel Epifanov <PEpifanov@inno.tech>
2024-09-24 11:08:21 +0300

    feat(working-schema): добавить variant к schema связанные поля DGR-243

--- shared/working-table/model/model.ts
+++ shared/working-table/model/model.ts
@@ -14,14 +14,13 @@ export type Schema = v.InferOutput<typeof schemaModel>
  * SchemaItem
  */
 
-export const schemaItemModel = v.object({
+export const schemaItemModelBase = v.object({
   id: v.string(),
   name: v.string(),
   columnName: v.string(),
   defaultTo: v.optional(v.string()),
   index: v.optional(v.boolean()),
   nullable: v.optional(v.boolean()),
-  type: v.string(),
   relation: v.optional(
     v.object({
       columnName: v.string(),
@@ -30,4 +29,73 @@ export const schemaItemModel = v.object({
   ),
 })
 
-export type SchemaItem = v.InferOutput<typeof schemaItemModel>
+// Добавляем варианты для type и связанных полей
+export const schemaItemModelVariants = v.variant('type', [
+  v.object({
+    type: v.literal('string'),
+    maxLength: v.number(),
+    decimalPlaces: v.optional(v.number()),
+    isNegativeAllowed: v.optional(v.boolean()),
+  }),
+  v.object({
+    type: v.literal('integer'),
+    decimalPlaces: v.optional(v.number()),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('number'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('float'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('byte'),
+    decimalPlaces: v.optional(v.number()),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('short'),
+    decimalPlaces: v.optional(v.number()),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('long'),
+    decimalPlaces: v.optional(v.number()),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('double'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('boolean'),
+    decimalPlaces: v.optional(v.number()),
+    maxLength: v.optional(v.number()),
+    isNegativeAllowed: v.optional(v.boolean()),
+  }),
+  v.object({
+    type: v.literal('date'),
+    decimalPlaces: v.optional(v.number()),
+    maxLength: v.optional(v.number()),
+    isNegativeAllowed: v.optional(v.boolean()),
+  }),
+])
+
+type SchemaItemBase = v.InferOutput<typeof schemaItemModelBase>
+type SchemaItemVariants = v.InferOutput<typeof schemaItemModelVariants>
+
+export const schemaItemModel = v.intersect([schemaItemModelBase, schemaItemModelVariants])
+
+export type SchemaItem = SchemaItemBase & SchemaItemVariants
