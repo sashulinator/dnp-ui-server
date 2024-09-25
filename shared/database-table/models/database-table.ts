6e1417fdcbd2d9ba0bd073857db25068f0fd1f97
Savchenko Alexander <asavchenko@datatech.ru>
2024-09-25 09:49:25 +0700

    refactor(databaseTable): порядок с зависимостями DGR-0

--- shared/database-table/models/database-table.ts
+++ shared/database-table/models/database-table.ts
@@ -4,28 +4,81 @@ import * as v from 'valibot'
  * DatabaseTable
  */
 
-export const DatabaseTableModel = v.object({
-  items: v.array(v.lazy(() => itemModel)),
+export const databaseTableModel = v.object({
+  items: v.array(v.lazy(() => columnModel)),
 })
 
-export type DatabaseTable = v.InferOutput<typeof DatabaseTableModel>
+export type DatabaseTable = v.InferOutput<typeof databaseTableModel>
 
 /**
- * Item
+ * Column
  */
 
-export const itemModel = v.object({
-  id: v.string(),
-  name: v.string(),
-  columnName: v.string(),
-  defaultTo: v.optional(v.string()),
-  index: v.optional(v.boolean()),
-  nullable: v.optional(v.boolean()),
-  type: v.string(),
-  relation: v.optional(v.lazy(() => relationModel)),
-})
+export const columnTypeModel = v.variant('type', [
+  v.object({
+    type: v.literal('string'),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('integer'),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('number'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('float'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('byte'),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('short'),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('long'),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('double'),
+    decimalPlaces: v.number(),
+    isNegativeAllowed: v.boolean(),
+    maxLength: v.number(),
+  }),
+  v.object({
+    type: v.literal('boolean'),
+  }),
+  v.object({
+    type: v.literal('date'),
+  }),
+])
 
-export type Item = v.InferOutput<typeof itemModel>
+export const columnModel = v.intersect([
+  columnTypeModel,
+  v.object({
+    id: v.string(),
+    name: v.string(),
+    columnName: v.string(),
+    defaultTo: v.optional(v.string()),
+    index: v.optional(v.boolean()),
+    nullable: v.optional(v.boolean()),
+    relation: v.optional(v.lazy(() => relationModel)),
+  }),
+])
+
+export type Column = v.InferOutput<typeof columnModel>
 
 /**
  * Relation
@@ -36,4 +89,4 @@ export const relationModel = v.object({
   kn: v.string(),
 })
 
-export type Relation = v.InferOutput<typeof itemModel>
+export type Relation = v.InferOutput<typeof columnModel>
