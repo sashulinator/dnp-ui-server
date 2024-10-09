d2c7a86fb93421525a057fb707dd3a4d8403057b
Savchenko Alexander <asavchenko@datatech.ru>
2024-10-09 12:04:26 +0700

    refactor(table): разнести column и list DGR-0

--- shared/where/lib/to-filter-config.ts
+++ shared/where/lib/to-filter-config.ts
@@ -1,14 +1,16 @@
+import { type Dictionary } from '~/utils/core'
+
 import { getKeys } from '../../dictionary'
 import { type ComparisonKey } from '../models/comparison'
 import { type FilterConfig } from '../models/filter-config'
-import { type IntFilter } from '../models/int-filter'
 import { INT_MODE } from '../models/int-mode'
-import { type IsFilter } from '../models/is-filter'
 import { MATCH } from '../models/match'
 import { MATCH_MODE } from '../models/match-mode'
-import type { StringFilter } from '../models/string-filter'
+import { type ReplaceValueByFilter } from '../models/where'
 
-export function toFilterConfig(filter: StringFilter | IntFilter | IsFilter): FilterConfig {
+export function toFilterConfig<T extends Dictionary>(
+  filter: ReplaceValueByFilter<T>[keyof T] | undefined,
+): FilterConfig {
   const ret: FilterConfig = {
     type: MATCH.startsWith,
     value: null,
