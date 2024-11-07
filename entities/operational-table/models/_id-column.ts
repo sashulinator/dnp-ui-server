49068220de99ceccd25402fd50a90c14808e58ae
Savchenko Alexander <asavchenko@datatech.ru>
2024-11-07 19:05:49 +0700

    refactor(database): переименовать name в display DGR-0

--- entities/operational-table/models/_id-column.ts
+++ entities/operational-table/models/_id-column.ts
@@ -2,7 +2,7 @@ import { type Column } from '../../../slices/working-table'
 
 export const _idColumn: Column = {
   id: '_id',
-  name: '_ID',
+  display: '_ID',
   columnName: '_id',
   type: 'increments',
 }
