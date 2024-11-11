82652adfa2125d666f327a0b851e5a77605a5601
Pavel Epifanov <PEpifanov@inno.tech>
2024-11-11 16:50:49 +0300

    feat(heap): создать страницу heap добавил api DGR-0

--- entities/heap/models.ts
+++ entities/heap/models.ts
@@ -1,27 +1,8 @@
-import * as v from 'valibot'
-
 /**
  * BaseHeap
  */
-
-export const baseHeapSchema = v.object({
-  name: v.string(),
-  description: v.string(),
-  data: v.looseObject({}),
-})
-
-/**
- * CreateHeapSchema
- */
-
-export const createHeapSchema = baseHeapSchema
-
-export type CreateHeapSchema = v.InferOutput<typeof createHeapSchema>
-
-/**
- * UpdateHeapSchema
- */
-
-export const updateHeapSchema = baseHeapSchema
-
-export type UpdateHeapSchema = v.InferOutput<typeof updateHeapSchema>
+export type Heap = {
+  name: string
+  description: string
+  data: unknown
+}
