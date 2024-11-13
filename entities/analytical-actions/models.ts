0dbc9bd0451fbfc7f51c78a807dd624cdd1e554b
Pavel Epifanov <PEpifanov@inno.tech>
2024-11-14 01:32:12 +0300

    feat(analitic-action): создать сущность DGR-338

---
+++ entities/analytical-actions/models.ts
@@ -0,0 +1,16 @@
+import * as v from 'valibot'
+
+/**
+ * AnalyticalActions
+ */
+
+export const analyticalActions = v.object({
+  id: v.number(),
+  description: v.string(),
+  name: v.string(),
+  display: v.string(),
+  group: v.string(),
+  isText: v.boolean(),
+  isInt: v.boolean(),
+  isDate: v.boolean(),
+})
